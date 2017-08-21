252d0a50a883e04e0e1f9bbd08c87c1b5aa773b8
Ross Wilson <me@rosswilson.co.uk>
2017-08-21 22:45:21 +0100

    Added test demoing req header issue

--- lib/example_web/controllers/page_controller.ex
+++ lib/example_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule ExampleWeb.PageController do
   use ExampleWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    token = case get_req_header(conn, "authorization") do
+      [token] -> token
+      _ -> "unknown"
+    end
+
+    text conn, "Authorization token is #{token}"
   end
 end
--- test/example_web/controllers/page_controller_test.exs
+++ test/example_web/controllers/page_controller_test.exs
@@ -1,8 +1,42 @@
 defmodule ExampleWeb.PageControllerTest do
   use ExampleWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  describe "single request" do
+    test "single request has the auth token", %{conn: conn} do
+      conn = conn
+      |> put_req_header("authorization", "secret")
+      |> get("/")
+
+      assert text_response(conn, 200) =~ "Authorization token is secret"
+    end
+  end
+
+  describe "two requests" do
+    test "only the first request has the auth token", %{conn: conn} do
+      conn = conn
+      |> put_req_header("authorization", "secret")
+      |> get("/")
+
+      assert text_response(conn, 200) =~ "Authorization token is secret"
+
+      conn = conn
+      |> get("/")
+
+      assert text_response(conn, 200) =~ "Authorization token is unknown"
+    end
+
+    test "can't set req headers on second request conn", %{conn: conn} do
+      conn = conn
+      |> put_req_header("authorization", "secret")
+      |> get("/")
+
+      assert text_response(conn, 200) =~ "Authorization token is secret"
+
+      assert_raise Plug.Conn.AlreadySentError, fn ->
+        conn
+        |> put_req_header("authorization", "secret")
+        |> get("/")
+      end
+    end
   end
 end
